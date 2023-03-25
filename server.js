774e82deff728cc8f189ac4db70751bc81595274
spacebanana123 <59102305+spacebanana123@users.noreply.github.com>
2023-03-25 17:24:44 -0500

    pug

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 
 const app = express();
 
+const path = require('path');
+
 app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index', { 
